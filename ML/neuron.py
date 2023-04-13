e3b1504919a2a6a70704d22e118ebf690af0bb85
Canopy <90562261+Canopy-PurpleSky@users.noreply.github.com>
2023-04-13 20:36:56 +0300

    removed unnecessary code

--- ML/neuron.py
+++ ML/neuron.py
@@ -13,11 +13,3 @@ class Neuron:
     def neuron_propage(self,input):
         activate = np.dot(input,self.weight) + self.bias
         return sigmoid(activate)
-
-weights = np.array([0,1])
-bias = 0
-x = np.array([2,3])
-
-n_1 = Neuron(bias,weights)
-
-print(n_1.neuron_propage(x))
