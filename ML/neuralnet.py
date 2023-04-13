b1bf5a479c39d345702858b33ae67a747a37f806
Canopy <90562261+Canopy-PurpleSky@users.noreply.github.com>
2023-04-13 20:37:47 +0300

    moved neuron class into its own file

--- ML/neuralnet.py
+++ ML/neuralnet.py
@@ -1,20 +1,6 @@
 import random
 import numpy as np
-
-
-def sigmoid(x):
-  #Sigmoid Squishification
-  return 1 / (1 + np.exp(-x))
-
-class Neuron:
-    def __init__(self,bias,weight):
-        self.bias = bias
-        self.weight = weight
-    
-    def neuron_propage(self,input):
-        activate = np.dot(input,self.weight) + self.bias
-        return sigmoid(activate)
-
+from neuron import Neuron
 
 class NeuralNet:
     def __init__(self):
